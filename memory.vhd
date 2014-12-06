de7fd084d6011ecf7ac9a92eef3deab6b9c21f12
zero <boy2000_007man@163.com>
2014-12-06 21:34:27 +0800

    add 0xBF00-0xBF01 COM support

--- memory.vhd
+++ memory.vhd
@@ -31,17 +31,20 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity MEMORY is
     Port ( Clk : in  STD_LOGIC;
-		   Rst : in  STD_LOGIC;
+    	     Rst : in  STD_LOGIC;
            RAM1_we : out  STD_LOGIC;
            RAM1_oe : out  STD_LOGIC;
            RAM1_en : out  STD_LOGIC;
            MEMop : in  STD_LOGIC_VECTOR (1 downto 0);
-		   addr : in  STD_LOGIC_VECTOR (15 downto 0);
-		   data : in  STD_LOGIC_VECTOR (15 downto 0);
+           addr : in  STD_LOGIC_VECTOR (15 downto 0);
+           data : in  STD_LOGIC_VECTOR (15 downto 0);
            memout : out  STD_LOGIC_VECTOR (15 downto 0);
            Clk2 : in  STD_LOGIC;
-		   COM_rdn : out  STD_LOGIC;
-	       COM_wrn : out  STD_LOGIC;
+           COM_rdn : out  STD_LOGIC;
+           COM_wrn : out  STD_LOGIC;
+           COM_tsre: in  STD_LOGIC;
+           COM_tbre: in  STD_LOGIC;
+           COM_data_ready: in  STD_LOGIC;
            RAM1_addr : out  STD_LOGIC_VECTOR (17 downto 0);
            RAM1_data : inout  STD_LOGIC_VECTOR (15 downto 0));
 end MEMORY;
@@ -50,35 +53,81 @@ architecture RTL of MEMORY is
 	signal step: std_logic := '0';
 begin
 
-	COM_rdn <= '1';
-	COM_wrn <= '1';
-	RAM1_en <= '0';
-	RAM1_oe <= '0';
-	RAM1_addr <= "00" & addr;
-	memout <= RAM1_data;
-	process (Clk2, Rst) begin
+	process (Clk2, Rst, addr) begin
 		if (Rst = '0') then
 			step <= '0';
+			RAM1_en <= '1';
+		   RAM1_oe <= '1';
+         RAM1_we <= '1';
+			COM_rdn <= '1';
+			COM_wrn <= '1';
+			RAM1_addr <= (others => '0');
+			RAM1_data <= (others => '0');
+			memout <= (others => '0');
 		elsif (Clk2'event and Clk2 = '1') then
 			step <= not step;
-			case MEMop is
-				when "10"=>
+         case addr(15 downto 1) is
+           	when "101111110000000"=>
+					RAM1_en <= '1';
+					RAM1_oe <= '1';
 					RAM1_we <= '1';
-					if (step = '0') then
-						RAM1_data <= (others => 'Z');
-					end if;
-				when "11"=>
-					RAM1_data <= data;
-					if (step = '0') then
-						RAM1_we <= '1';
+					RAM1_addr <= (others => '0');
+					if addr(0) = '0' then
+						memout <= RAM1_data;
+						case MEMop is
+							when "10"=>
+								COM_wrn <= '1';
+								if (step = '0') then
+									COM_rdn <= '1';
+									RAM1_data <= (others => 'Z');
+								else
+									COM_rdn <= '0';
+								end if;
+							when "11"=>
+								COM_rdn <= '1';
+								RAM1_data <= data;
+								if (step = '0') then
+									COM_wrn <= '1';
+								else
+									COM_wrn <= '0';
+								end if;
+							when others=>
+								COM_rdn <= '1';
+								COM_wrn <= '1';
+								RAM1_data <= (others => '0');
+						end case;
 					else
-						RAM1_we <= '0';
+						COM_rdn <= '1';
+						COM_wrn <= '1';
+						RAM1_data <= (others => 'Z');
+						memout <= "00000000000000"&COM_data_ready&(COM_tsre and COM_tbre);
 					end if;
-				when others=>
-					RAM1_we <= '1';
-			end case;
+            when others=>
+					RAM1_en <= '0';
+		    		RAM1_oe <= '0';
+					COM_rdn <= '1';
+					COM_wrn <= '1';
+					RAM1_addr <= "00"&addr;
+					memout <= RAM1_data;
+					case MEMop is
+						when "10"=>
+							RAM1_we <= '1';
+							if (step = '0') then
+								RAM1_data <= (others => 'Z');
+							end if;
+						when "11"=>
+							RAM1_data <= data;
+							if (step = '0') then
+								RAM1_we <= '1';
+							else
+								RAM1_we <= '0';
+							end if;
+						when others=>
+							RAM1_we <= '1';
+                    		RAM1_data <= (others => '0');
+					end case;
+            end case;
 		end if;
 	end process;
 	
 end RTL;
-
